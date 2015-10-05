991b654772e9de5a7ecffaa6a2cb52d30278ad6e
Matt Schneeberger <matt@unifyingsolution.com>
2015-10-04 23:09:16 -0500

    Moved watch face to own module

--- watchface/src/main/java/com/schneenet/android/weatherwatch/watchface/WeatherWatchFace.java
+++ watchface/src/main/java/com/schneenet/android/weatherwatch/watchface/WeatherWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.schneenet.android.weatherwatch;
+package com.schneenet.android.weatherwatch.watchface;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
